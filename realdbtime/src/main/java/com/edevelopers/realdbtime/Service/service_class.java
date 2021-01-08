2bd72a51d933c630d4c2edd897b1b61a6ad46cb6
anubhavsingh0100 <anubhavsingh0100@gmail.com>
2021-01-08 16:48:57 +0530

    Edevlopers

--- realdbtime/src/main/java/com/edevelopers/realdbtime/Service/service_class.java
+++ realdbtime/src/main/java/com/edevelopers/realdbtime/Service/service_class.java
@@ -29,6 +29,13 @@ public class service_class {
         void onError(String Error);
     }
 
+    public interface Callbackres
+    {
+        void onSuccess(String Result);
+
+        void onError(String Error);
+    }
+
 
     public static void RequestData(ModelClass Modeldata, final Callback callback){
         RequestQueue queue= Volley.newRequestQueue(Modeldata.getContext());
@@ -53,10 +60,14 @@ public class service_class {
                     try{
                         for(int i = 0;i < response.length();i++){
                             JSONObject explrObject = response.getJSONObject(i);
-                            fed.add (new DBColumnResult(
-                                    dbcol.get(i).getColumnname(),
-                                    explrObject.getString(dbcol.get(i).getColumnname())
-                            ));
+                            try{
+                                fed.add (new DBColumnResult(
+                                        dbcol.get(i).getColumnname(),
+                                        explrObject.getString(dbcol.get(i).getColumnname())
+                                ));
+                            }catch (Exception e){
+                                callback.onError(explrObject.getString("Error"));
+                            }
                         }
                     }
                     catch (Exception e){
@@ -77,4 +88,54 @@ public class service_class {
             Log.d(Const.TAG,""+e.getMessage());
         }
     }
+
+    public static void SaveData(ModelClass Modeldata, final Callbackres callback){
+        RequestQueue queue= Volley.newRequestQueue(Modeldata.getContext());
+        String url = apiurl+"getresult";
+       // final ArrayList<DBColumn> dbcol = Modeldata.getDbcolumn();
+
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.accumulate(Const.api_key, Modeldata.getApi_key());
+            jsonObject.accumulate(Const.appname, Modeldata.getAppname());
+            jsonObject.accumulate(Const.api_secret, Modeldata.getApi_secret());
+            jsonObject.accumulate(Const.query, Modeldata.getQuery());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        try{
+            customRequest jsonObjectRequest = new customRequest(Request.Method.POST, url, jsonObject, new Response.Listener<JSONArray>() {
+                @Override
+                public void onResponse(JSONArray response) {
+                    String result = "";
+                    try{
+                        for(int i = 0;i < response.length();i++){
+                            JSONObject explrObject = response.getJSONObject(i);
+                            try{
+                                result = explrObject.getString("result");
+                            }catch (Exception e){
+                                result = explrObject.getString("error");
+                            }
+                        }
+                    }
+                    catch (Exception e){
+                        callback.onError(e.getMessage());
+                    }
+
+                    callback.onSuccess(result);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    callback.onError(error.getMessage());
+                }
+            });
+
+            queue.add(jsonObjectRequest);
+        }catch (Exception e) {
+            Log.d(Const.TAG,""+e.getMessage());
+        }
+    }
+
 }
